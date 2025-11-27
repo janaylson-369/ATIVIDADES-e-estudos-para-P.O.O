ebe346e7b70710b736b9c2cc7dc3477ef6b61516
Janailson Gomes <janailson.gomes@academico.ifpb.edu.br>
2025-11-26 22:20:57 -0300

    Principal.java

--- JAVA_ATIVIDADES/atividademercadinho/Principal.java
+++ JAVA_ATIVIDADES/atividademercadinho/Principal.java
@@ -6,9 +6,9 @@ public class Principal {
 
         Mercadinho m = new Mercadinho();
 
-        m.adicionarCliente(new ClienteRegular("Ana", 100.0));
-        m.adicionarCliente(new ClienteVip("Carlos", 200.0, "1234-5678"));
-        m.adicionarCliente(new ClienteOuroVip("Maria", 500.0, "9999-8888", "Rua Central, 45"));
+        m.adicionarCliente(new ClienteRegular("Ana", 325.0));
+        m.adicionarCliente(new ClienteVip("Carlos", 6428.0, "98995494"));
+        m.adicionarCliente(new ClienteOuroVip("Maria", 6451.0, "5115151", "Rua do medo"));
 
         m.imprimirClientes();
         System.out.println("\nvalor total das compras de todos os clientes: R$ " + m.calcularTotal());
